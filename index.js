e4f26a096d3eb9b3bce0e2138745ec5f66ddee56
nimkus <164895942+nimkus@users.noreply.github.com>
2024-09-16 16:27:03 +0200

    Remove passport.authenticate('jwt', { session: false }), from /movies endpoint

--- index.js
+++ index.js
@@ -169,7 +169,17 @@ app.get('/', (req, res) => {
 
 // READ – Get a list
 // of all movies
-listAll('/movies', movies, 'genre director actors');
+//listAll('/movies', movies, 'genre director actors');
+// List all items of a db collection
+app.get(routePath, async (req, res) => {
+  try {
+    const list = await movies.find().populate('genre director actors');
+    res.status(201).json(list);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  }
+});
 // of all genres
 listAll('/movies/genres/all', genres);
 // of all directors
